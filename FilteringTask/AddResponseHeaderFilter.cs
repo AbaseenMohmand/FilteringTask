20146319a3a72eb0f39e93520fa9cc56cc8ac23f
AbaseenMohmand <abaseenkhanz448@gmail.com>
2022-05-30 04:31:57 -0700

    Add project files.

---
+++ FilteringTask/AddResponseHeaderFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace FilteringTask
+{
+    public class AddResponseHeaderFilter : IActionFilter
+    {
+      
+
+        public  void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+         
+        }
+
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            string ActionName = context.RouteData.Values["action"].ToString();
+            context.HttpContext.Response.Headers.Add("Action", ActionName);
+
+            string HTTPMethod = context.HttpContext.Request.Method;
+            context.HttpContext.Response.Headers.Add("HTTPMethod", HTTPMethod);
+
+            string httpScheme = context.HttpContext.Request.Scheme;
+            context.HttpContext.Response.Headers.Add("httpScheme", httpScheme);
+
+            string host = context.HttpContext.Request.Host.ToString();
+            context.HttpContext.Response.Headers.Add("host", host);
+
+            string port = context.HttpContext.Request.Host.Port.ToString();
+            context.HttpContext.Response.Headers.Add("port", port);
+
+            var watch = new Stopwatch();
+            watch.Start();
+            Task.Delay(1000).Wait();
+            var responseTimeForCompleteRequest = watch.ElapsedMilliseconds;
+            context.HttpContext.Response.Headers.Add("TimeTakenToExecute", responseTimeForCompleteRequest.ToString());
+            watch.Stop();
+
+            string ServerDateAndTime = DateTime.Now.ToString();
+            context.HttpContext.Response.Headers.Add("ServerDateAndTime", ServerDateAndTime);
+        }
+    }
+}
---
+++ FilteringTask/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using Google.Api.Ads.AdWords.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FilteringTask.Controllers
+{
+    [Route("api/[controller]")]
+   // [ApiController]
+        
+
+    public class TaskController : ControllerBase
+    {
+
+        [HttpGet]
+        [ServiceFilter(typeof(AddResponseHeaderFilter))]
+        public IActionResult index()
+        {
+            // Generate dummy values
+         return Ok();
+        }
+    }
+}
